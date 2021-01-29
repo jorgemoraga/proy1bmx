61fe9650d43ebe7e37525b712e8c4b063bf88a4c
ignacioopazo <77399296+ignacioopazo@users.noreply.github.com>
2021-01-29 01:13:20 -0300

    Add files via upload

---
+++ app/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Contacto, Bicicleta
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+#formulario para contacto
+class ContactoForm(forms.ModelForm):
+    class Meta:
+        model = Contacto
+        fields = ["nombre", "correo", "tipo_consulta", "mensaje", "avisos" ]
+
+
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Bicicleta
+        fields = '__all__'
+
+
+
+class CustomUserCreationForm(UserCreationForm):   
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', "last_name", "email", "password1", "password2"]
