5f920e78e17ced5cb7a026bab2e8a4a1b5bf52f8
ignacioopazo <77399296+ignacioopazo@users.noreply.github.com>
2021-01-29 00:57:00 -0300

    Delete app directory

--- app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Categoria, Bicicleta, Contacto
-# Register your models here.
-
-#incluimos los modelos en el dominio del admin
-admin.site.register(Categoria)
-admin.site.register(Bicicleta)
-admin.site.register(Contacto)
